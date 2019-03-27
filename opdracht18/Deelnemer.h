0ee5e621157aa09ecc76db72c10a3877dd974d0e
DevonThomassen <devon.thomassen@hotmail.com>
2019-03-27 10:20:46 +0100

    Added exercise 18 🌞

--- opdracht18/Deelnemer.h
+++ opdracht18/Deelnemer.h
@@ -1,1 +1,48 @@
-#pragma once
+/************************** Module Header *******************************\
+Opdracht:       opdracht18
+Auteur:			Devon Thomassen
+Aanmaakdatum:   20-03-2019 11:00
+Bestandsnaam:   Deelnemer.h
+Versie:         0.1
+				0.2
+				1.0
+
+\************************************************************************/
+
+// Declaratie 
+class Deelnemer
+{
+private:
+	string deelnemerNaam;
+
+public:
+	int rugNummerChipnummer[2];
+
+	// Constructor
+	Deelnemer()
+	{
+
+	};
+
+	// Methoden
+	void Invoeren();
+	void Weergeven();
+};
+
+// Implementatie
+void Deelnemer::Invoeren()
+{
+	//Initialiseren
+	cout << "Voer deelnemernaam in: ";
+	cin >> deelnemerNaam;
+	cout << "Voer rugnummer in: ";
+	cin >> rugNummerChipnummer[0];
+	cout << "Voer chipnummer in: ";
+	cin >> rugNummerChipnummer[1];
+};
+
+void Deelnemer::Weergeven()
+{
+	// UI
+	cout << "Deelnemer " << deelnemerNaam << " heeft het rugnummer " << rugNummerChipnummer[0] << " en het chipnummer " << rugNummerChipnummer[1] << endl;
+};
--- opdracht18/opdracht18.cpp
+++ opdracht18/opdracht18.cpp
@@ -2,7 +2,7 @@
 Opdracht:       opdracht18
 Auteur:			Devon Thomassen
 Aanmaakdatum:   20-03-2019 11:00
-Bestandsnaam:   opdracht16.cpp
+Bestandsnaam:   opdracht18.cpp
 Versie:         0.1
 				0.2
 				1.0
@@ -14,9 +14,15 @@ Versie:         0.1
 
 using namespace std;
 
+#include "Deelnemer.h"
+
 int main()
 {
 	// Constructie
+	Deelnemer deelnemer1;
 
-	// Initialisatie
+	// Functieaanroep
+	deelnemer1.Invoeren();
+	deelnemer1.Weergeven();
+	system("pause");
 }
